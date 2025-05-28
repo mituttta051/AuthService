365131563ded78a05418a6c6541d5ae56ec50487
mituttta051 <amityutneva@mail.ru>
2025-05-28 19:44:09 +0300

    initial commit

---
+++ src/test/java/cybercooker/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cybercooker.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
